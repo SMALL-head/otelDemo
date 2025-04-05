da90af6e2c7edd1f659f821747eeb85824a61760
zyc <skyzyc@126.com>
2025-04-05 17:04:14 +0800

    tmp: otel-gin tmp commit

---
+++ otelgin/common/consts/const.go
@@ -0,0 +1,5 @@
+package consts
+
+const (
+	CybertwinKey = "cybertwin_id"
+)
--- otelgin/main.go
+++ otelgin/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	""
+	"github.com/gin-gonic/gin"
+	"otelDemo/otelgin/core"
+)
 
+func main() {
+	server := core.NewOtelGinServer(gin.ReleaseMode, "test111")
+	if err := server.Run(":8080"); err != nil {
+		logrus.Fatal(err)
+	}
 }
